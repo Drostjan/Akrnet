de82242b6411c37c8486131c655dc9228cf0861f
Drostjan <androsales.17@gmail.com>
2022-09-27 15:55:36 +0200

    A Under Float Game in Rust

--- target/debug/deps/arknet-8595799568aeee22.d
+++
@@ -1,8 +0,0 @@
-/home/drostjan/Src/Akrnet/target/debug/deps/arknet-8595799568aeee22.rmeta: src/main.rs src/player.rs src/player/ship.rs src/player/board.rs
-
-/home/drostjan/Src/Akrnet/target/debug/deps/arknet-8595799568aeee22.d: src/main.rs src/player.rs src/player/ship.rs src/player/board.rs
-
-src/main.rs:
-src/player.rs:
-src/player/ship.rs:
-src/player/board.rs:
--- target/debug/deps/arknet-f7dbac1c77bc18a3.d
+++
@@ -1,8 +0,0 @@
-/home/drostjan/Src/Akrnet/target/debug/deps/arknet-f7dbac1c77bc18a3.rmeta: src/main.rs src/player.rs src/player/ship.rs src/player/board.rs
-
-/home/drostjan/Src/Akrnet/target/debug/deps/arknet-f7dbac1c77bc18a3.d: src/main.rs src/player.rs src/player/ship.rs src/player/board.rs
-
-src/main.rs:
-src/player.rs:
-src/player/ship.rs:
-src/player/board.rs:
